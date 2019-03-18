d4e5e6f69919f2c942d37f6168e3d70c5c1e0c23
Seok Lee <seokleeuk@gmail.com>
2019-03-18 11:46:18 -0400

    Add files via upload
    /*
    without using namespace std, the declaration of datatype of string is, std::string
    */
    
    cin<<

---
+++ ex_1_33_cpp_user_inputs/ex_1_33_cpp_user_inputs/ex_1_333_cpp_user_inputs.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{
+	int year = 0;
+	int age = 0;
+	string name = "";
+	cout << "what year is your favorite?\n";
+	cin >> year;
+	cout << "how interesting, your favorite year is " << year << endl;
+
+	return 0;
+}
+
+/*
+without using namespace std, the declaration of datatype of string is, std::string
+*/
+
+
