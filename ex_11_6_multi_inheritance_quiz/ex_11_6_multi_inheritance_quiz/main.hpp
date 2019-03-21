a92a72cd80e7d6e64506aa2b2f751aef5edee162
Seok Lee <seokleeuk@gmail.com>
2019-03-21 12:08:06 -0400

    Add files via upload

---
+++ ex_11_6_multi_inheritance_quiz/ex_11_6_multi_inheritance_quiz/main.hpp
@@ -0,0 +1,76 @@
+//header file for main.cpp
+/*TODO:
+*-  create a class called Pet
+*- it inherits from both the class Patient
+*- and the class Dog
+*- Pet has one private member: name
+*- Pet has public getName and setName functions.
+*/
+#define _CRT_SECURE_NO_WARNINGS
+#include<iostream>
+#include<string>
+using namespace std;
+
+class Patient
+{
+private:
+	int idNumber;
+public:
+	void setIdNumber(int idIn);
+	int getIdNumber();
+};
+
+void Patient::setIdNumber(int idIn)
+{
+	idNumber = idIn;
+}
+
+int Patient::getIdNumber()
+{
+	return idNumber;
+}
+
+class Dog
+{
+private:
+	string breed;
+public:
+	void setBreed(string breedIn);
+	string getBreed();
+};
+
+void Dog::setBreed(string breedIn)
+{
+	breed = breedIn;
+}
+
+string Dog::getBreed()
+{
+	return breed;
+}
+
+class Pet : public Patient, public Dog
+{
+private:
+	string name;
+public:
+	Pet();
+	string getName();
+	void setName(string InName);
+};
+
+
+Pet::Pet()
+{
+	name = "NA";
+}
+
+string Pet::getName()
+{
+	return name;
+}
+
+void Pet::setName(string InName)
+{
+	name = InName;
+}
